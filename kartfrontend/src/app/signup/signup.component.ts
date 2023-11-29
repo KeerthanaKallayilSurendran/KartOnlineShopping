6c4e0e08805d15110f04096492e756eec4f9ce5f
KeerthanaKallayilSurendran <keerthana.kallayilsurendran@gmail.com>
2023-11-29 18:56:05 +0530

    frontend signupformservice

--- kartfrontend/src/app/signup/signup.component.ts
+++ kartfrontend/src/app/signup/signup.component.ts
@@ -1,71 +1,93 @@
-import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl, ReactiveFormsModule, Validator } from "@angular/forms";
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
-import {MatSelectModule} from '@angular/material/select';
-import {MatIconModule} from '@angular/material/icon';
-import { Validators } from "@angular/forms";
-import { CommonModule } from "@angular/common";
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { SignUpService } from './signup.service';
+import { HttpClientModule } from '@angular/common/http';
 
 interface Gender {
   value: string;
   viewValue: string;
 }
 
-
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss'],
-  imports: [MatButtonModule, MatInputModule, MatFormFieldModule, FormsModule, MatSelectModule, MatIconModule, ReactiveFormsModule, CommonModule]
+  providers: [SignUpService],
+  imports: [
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatSelectModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    CommonModule,
+  ],
 })
-export class SignupComponent implements OnInit{
-
+export class SignupComponent implements OnInit {
   reactiveForm: FormGroup;
-  
+
+  constructor(
+    private signUpService: SignUpService
+    ) {}
+
   ngOnInit(): void {
     this.reactiveForm = new FormGroup({
       username: new FormControl(null, [
         Validators.required,
         Validators.minLength(3),
-        Validators.maxLength(20)
+        Validators.maxLength(20),
       ]),
-      email: new FormControl(null, [
-        Validators.required,
-        Validators.email
-      ]),
-      mobile: new FormControl(null,[
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      mobile: new FormControl(null, [
         Validators.required,
         Validators.minLength(10),
-        Validators.maxLength(10)
+        Validators.maxLength(10),
       ]),
-      gender: new FormControl(null,[
-        Validators.required
-      ]),
-      password: new FormControl(null,[
+      gender: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [
         Validators.required,
         // Validators.pattern(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])$/)
-        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&-+=])([a-zA-Z\d@#$%^&-+=]{8,25})$/
-        )
+        Validators.pattern(
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&-+=])([a-zA-Z\d@#$%^&-+=]{8,25})$/
+        ),
       ]),
-      userconfirmpassword: new FormControl(null,[
-        Validators.required
-      ])
-    })
+      userconfirmpassword: new FormControl(null, [Validators.required]),
+    });
   }
 
   genders: Gender[] = [
-    {value: 'female', viewValue: 'Female'},
-    {value: 'male', viewValue: 'Male'}
+    { value: 'female', viewValue: 'Female' },
+    { value: 'male', viewValue: 'Male' },
   ];
   hide = true;
 
-  onFormSubmitted(){
+  onFormSubmitted() {
     console.log(this.reactiveForm.value);
+    const values = this.reactiveForm.value;
+    this.signUpService.connect().subscribe(
+      {
+        next: data => {
+          console.log(data, 85);
+        },
+        error: err => {
+          console.log(err, 55);
+        }
+      }
+    )
   }
 }
-
-
