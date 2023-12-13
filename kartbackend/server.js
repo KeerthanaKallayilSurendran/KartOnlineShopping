2c0fb1c19ccad7d3e19c59fa13671d784829b799
KeerthanaKallayilSurendran <keerthana.kallayilsurendran@gmail.com>
2023-12-13 11:47:26 +0530

    signin completed

--- kartbackend/server.js
+++ kartbackend/server.js
@@ -1,14 +1,24 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt')
 const express = require('express');
 const app = express();
-const port = 3000; 
+const port = 3000;
 const cors = require('cors');
 const morgan = require('morgan');
 const { dbConnect } = require("./mongodb");
 const { User } = require("./Modals/userSchema");
+const session = require('express-session')
+const passport = require('passport');
+require("./auth")(passport);
 const SALT_ROUNDS = 5
-console.log(6); 
 
+dbConnect();
+
+// app.use(flash);
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}));
 app.use(cors({
   origin: 'http://localhost:4200'
 }));
@@ -17,49 +27,80 @@ app.use(morgan('dev'))
 // Add these two lines
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize())
+app.use(passport.session())
 
-dbConnect();
 
-app.post('/signup', async(req, res) => {
-  console.log(758851)
-  const signupFormData = {
-    username: req.body.username,
-    email: req.body.email,
-    mobile: req.body.mobile,
-    gender: req.body.gender,
-    password: await bcrypt.hash(req.body.password, SALT_ROUNDS) ,
-  };
+
+
+
+
+app.post('/signup', async (req, res) => {
   try {
-    console.log(27);
-    const userData = await User.create(signupFormData);
-    console.log(userData);
-  } catch (error) {
-    console.log(error.message);
-  }
-  res.json({ message: 'hello' })
-});
+    const { email, username, mobile, gender, password: userPassword } = req.body
+    const password = await bcrypt.hash(userPassword, SALT_ROUNDS)
 
-console.log(40);
-
-app.post('/signin', async(req, res) => {
-
-  try {
-    const check = await User.findOne({ email: req.body.email});
-    if(check.password == req.body.password){
-      res.json({ message: 'hello'})
+    const succ = await User.findOne({ email })
+    if (succ) {
+      return res.json({ message: 'User already exists' })
     }
-  } catch (error) {
-    console.log(error.message);
-  }
+    const signupFormData = {
+      username,
+      email,
+      mobile,
+      gender,
+      password
+    };
+    const userData = await User.create(signupFormData);
 
- 
+    res.json({ message: 'Signup successfully' })
+  } catch (e) {
+    console.log(e.message);
+  }
 });
 
 
+app.post('/signin', async (req, res) => {
+  try {
+    const { email, password: userPassword } = req.body
+    const user = await User.findOne({ email })
+    const { password } = user
+    if (user) {
+      const isMatch = bcrypt.compare(password, userPassword)
+      if (isMatch) {
+        return res.json({ message: 'Signin successfully', success: true })
+      }
+      return res.json({ message: 'Signin Failed', success: false })
+    }
+    
+    res.json({ message: 'user not existing', success: false })
+  } catch (err) {
+    res.json({ message: err.message });
+  }
+})
+
+
+
+
+
+// app.post("/signin", (req, res) => {
+//   console.log('65')
+//   passport.authenticate("local", (err, user, info) => {
+//     console.log(67)
+//     if (err) throw err;
+//     if (!user) res.json({ message: 'No User Exists'});
+//       req.logIn(user, (err) => {
+//         if (err) throw err;
+//         res.json({ message: 'Successfully Authenticated'});
+//         console.log(req.user);
+//       });
+//     }
+//   });
+// });
 
 
 app.listen(port, () => {
-    console.log(`Server stareted`);
+  console.log(`Server started`, port);
 });
 
 
